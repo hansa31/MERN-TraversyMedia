3f173f3ffe142e159a8a49a9e2c4d969759baf30
hansa31 <hansaalahakoon@gmail.com>
2021-06-05 21:03:55 +0530

    .gitignore is now working

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://Hansa:Hansa1234@cluster0.m1j5g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-}
-
-//As for actually using the "new" connection URI options, the main restriction is to actually include the port on the connection string:
